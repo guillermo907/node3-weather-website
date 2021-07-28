9ff01f01045c531e6824a393b3498be684418940
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2021-07-27 20:50:10 -0500

    addional info added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,6 +4,8 @@ const forecast = (longitude, latitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=70a3ef78a34ac6826ce6f7e1046976e0&query=${latitude},${longitude}`;
 
   request({ url, json: true }, (error, { body }) => {
+    console.log('Datos: ', body);
+
     if (error) {
       callback(error, undefined);
     } else if (body.error) {
@@ -11,7 +13,8 @@ const forecast = (longitude, latitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}, It is ${body.current.temperature} degrees outside and is ${body.current.humidity}% humid!`
+        `${body.current.weather_descriptions[0]}, It is ${body.current.temperature} degrees outside and is 
+        ${body.current.humidity}% humid! Its ${body.location.localtime} in ${body.location.name} right now`
       );
     }
   });
